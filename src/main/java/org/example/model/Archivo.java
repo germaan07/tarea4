03d1d76c2f138d644306fae82cc541fc55f3cd4c
germaan07 <ortep09@gmail.com>
2026-01-29 14:28:46 +0100

    Second Commit

--- src/main/java/org/example/model/Archivo.java
+++ src/main/java/org/example/model/Archivo.java
@@ -7,7 +7,22 @@ import lombok.Data;
 @AllArgsConstructor
 
 public abstract class Archivo{
-    private int size;
-    private String path;
+    protected int size;
+    protected String path;
 
+    public void open(){
+        System.out.println("NOMBRE: ");
+        System.out.println("TAMAÑO: " + size);
+        System.out.println("RUTA: " + path);
+    }
+
+    public void move(String newPath){
+        if (path.equals(newPath)){
+            System.out.println("No hay cambios.");
+        } else {
+            path = newPath;
+            System.out.println("Se ha movido el fichero a " + newPath);
+        }
+
+    }
 }
